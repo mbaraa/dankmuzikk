912813e6fb138418630d8e5e2b9c1a3cb0386679
Baraa Al-Masri <pub@mbaraa.com>
2024-04-22 22:10:20 +0300

    chore(colors): add colorscheme to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,36 +9,13 @@ module.exports = {
       },
       colors: {
         primary: {
-          DEFAULT: "#433C36",
-          light: "#F9E6E6",
-        },
-        primary2: {
-          DEFAULT: "#615252",
-          light: "#E2BCBD",
+          DEFAULT: "#7ACB65",
         },
         secondary: {
-          DEFAULT: "#9A7272",
-          light: "#EF766B",
-        },
-        secondary2: {
-          DEFAULT: "#C37474",
-          light: "#C05B51",
+          DEFAULT: "#9EE07E",
         },
         accent: {
-          DEFAULT: "#C05B51",
-          light: "#C37474",
-        },
-        accent2: {
-          DEFAULT: "#EF766B",
-          light: "#9A7272",
-        },
-        accent3: {
-          DEFAULT: "#E2BCBD",
-          light: "#615252",
-        },
-        accent4: {
-          DEFAULT: "#F9E6E6",
-          light: "#433C36",
+          DEFAULT: "#4C8C36",
         },
       },
     },
