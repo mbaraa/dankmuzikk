ef3d482b5ac0cd5ba2982e75094321399a76fe80
Baraa Al-Masri <pub@mbaraa.com>
2024-04-30 01:04:23 +0300

    chore(search): response with `nil` to hide empty query

--- handlers/search_suggestions.go
+++ handlers/search_suggestions.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"context"
 	"dankmuzikk/components/ui/search"
+	"dankmuzikk/log"
 	"dankmuzikk/services/youtube"
 	"net/http"
 )
@@ -10,14 +11,21 @@ import (
 func HandleSearchSugessions(hand *http.ServeMux) {
 	hand.HandleFunc("/api/search-suggession", func(w http.ResponseWriter, r *http.Request) {
 		q := r.URL.Query().Get("query")
+		if q == "" {
+			w.Write(nil)
+			return
+		}
+
 		suggessions, err := youtube.SearchSuggestions(q)
 		if err != nil {
+			log.Warningln(err)
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
 
 		err = search.SearchSuggestions(suggessions, q).Render(context.Background(), w)
 		if err != nil {
+			log.Warningln(err)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
