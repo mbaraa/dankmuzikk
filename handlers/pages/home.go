dd7c6a8b068f47f6b5f03b7e1acf8d421ad67a88
Baraa Al-Masri <pub@mbaraa.com>
2024-05-02 09:13:26 +0300

    fix(pages): add content-type html

--- handlers/pages/home.go
+++ handlers/pages/home.go
@@ -13,6 +13,13 @@ func HandleHomePage(w http.ResponseWriter, r *http.Request) {
 	pages.Index(isMobile(r)).Render(context.Background(), w)
 }
 
+func Handler(hand http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		hand(w, r)
+	}
+}
+
 func isMobile(r *http.Request) bool {
 	return strings.Contains(strings.ToLower(r.Header.Get("User-Agent")), "mobile")
 }
