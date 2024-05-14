6c64d5acbd98b407d349d859b6512a9037d48ec4
Baraa Al-Masri <pub@mbaraa.com>
2024-05-14 10:37:46 +0300

    chore(ytdl): utilize the new download multi queue

--- services/youtube/download/download.go
+++ services/youtube/download/download.go
@@ -10,6 +10,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 )
 
 // Service represents the YouTube downloader service.
@@ -48,6 +49,78 @@ func (d *Service) DownloadYoutubeSong(req entities.SongDownloadRequest) error {
 		ThumbnailUrl: req.ThumbnailUrl,
 		Duration:     req.Duration,
 	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// DownloadYoutubeSongQueue same as DownloadYoutubeSong but it downloads the song in the background
+func (d *Service) DownloadYoutubeSongQueue(req entities.SongDownloadRequest) error {
+	path := fmt.Sprintf("%s/%s.mp3", config.Env().YouTube.MusicDir, req.Id)
+	if _, err := os.Stat(path); err == nil {
+		log.Infof("The song with id %s is already downloaded\n", req.Id)
+		return nil
+	}
+
+	resp, err := http.Get(fmt.Sprintf("%s/download/queue/%s", config.Env().YouTube.DownloaderUrl, req.Id))
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return errors.New("something went wrong when downloading a song; id: " + req.Id)
+	}
+
+	err = d.repo.Add(&models.Song{
+		YtId:         req.Id,
+		Title:        req.Title,
+		Artist:       req.Artist,
+		ThumbnailUrl: req.ThumbnailUrl,
+		Duration:     req.Duration,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// DownloadYoutubeSongQueue same as DownloadYoutubeSong but it downloads the song in the background
+func (d *Service) DownloadYoutubeSongQueueMulti(req []entities.SongDownloadRequest) error {
+	ids := make([]string, 0)
+	newSongs := make([]*models.Song, 0)
+	for _, song := range req {
+		path := fmt.Sprintf("%s/%s.mp3", config.Env().YouTube.MusicDir, song.Id)
+		if _, err := os.Stat(path); err == nil {
+			log.Infof("The song with id %s is already downloaded\n", song.Id)
+			continue
+		}
+
+		ids = append(ids, song.Id)
+		newSongs = append(newSongs, &models.Song{
+			YtId:         song.Id,
+			Title:        song.Title,
+			Artist:       song.Artist,
+			ThumbnailUrl: song.ThumbnailUrl,
+			Duration:     song.Duration,
+		})
+	}
+
+	resp, err := http.Get(
+		fmt.Sprintf("%s/download/queue/multi/%s",
+			config.Env().YouTube.DownloaderUrl,
+			strings.Join(ids, ","),
+		),
+	)
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return errors.New("something went wrong when downloading the songs: " + strings.Join(ids, ","))
+	}
+
+	err = d.repo.AddMany(newSongs)
 	if err != nil {
 		return err
 	}
