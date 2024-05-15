bfab21be970ac610a1296e9971fe2adb89e4c55b
Baraa Al-Masri <pub@mbaraa.com>
2024-05-15 23:44:59 +0300

    chore(playlist): add increment song plays in playlist

--- services/playlists/songs/songs.go
+++ services/playlists/songs/songs.go
@@ -42,6 +42,58 @@ func (s *Service) AddSongToPlaylist(songId, playlistPubId string) error {
 	})
 }
 
+// IncrementSongPlays increases the song's play times in the given playlist.
+// Checks for the song and playlist first, yada yada...
+// TODO: check playlist's owner :)
+func (s *Service) IncrementSongPlays(songId, playlistPubId string) error {
+	var song models.Song
+	err := s.
+		songRepo.
+		GetDB().
+		Model(new(models.Song)).
+		Select("id").
+		Where("yt_id = ?", songId).
+		First(&song).
+		Error
+	if err != nil {
+		return err
+	}
+
+	var playlist models.Playlist
+	err = s.
+		songRepo.
+		GetDB().
+		Model(new(models.Playlist)).
+		Select("id").
+		Where("public_id = ?", playlistPubId).
+		First(&playlist).
+		Error
+	if err != nil {
+		return err
+	}
+
+	var ps models.PlaylistSong
+	err = s.
+		playlistSongRepo.
+		GetDB().
+		Model(new(models.PlaylistSong)).
+		Select("play_times").
+		Where("playlist_id = ? AND song_id = ?", playlist.Id, song.Id).
+		First(&ps).
+		Error
+	if err != nil {
+		return err
+	}
+
+	return s.
+		playlistSongRepo.
+		GetDB().
+		Model(new(models.PlaylistSong)).
+		Where("playlist_id = ? AND song_id = ?", playlist.Id, song.Id).
+		Update("play_times", ps.PlayTimes+1).
+		Error
+}
+
 // RemoveSongFromPlaylist removes a given song from the given playlist,
 // checks if the actual song and playlist exist then removes the song to the given playlist,
 // and returns an occurring error.
