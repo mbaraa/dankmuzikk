e16be790ed9393c29c2cd0fb52231bb118074283
Baraa Al-Masri <pub@mbaraa.com>
2024-05-17 01:41:54 +0300

    chore(theme): fix themes' color groups

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,28 +9,22 @@ module.exports = {
       colors: {
         primary: {
           DEFAULT: "var(--primary-color)",
+          trans: {
+            20: "var(--primary-color-20)",
+          },
         },
         secondary: {
           DEFAULT: "var(--secondary-color)",
+          trans: {
+            20: "var(--secondary-color-20)",
+          },
         },
         accent: {
           DEFAULT: "var(--accent-color)",
-        },
-        black: {
-          DEFAULT: "#000",
           trans: {
-            100: "#00000044",
-            200: "#00000055",
-            300: "#00000066",
-            400: "#00000088",
-            500: "#000000B0",
-          },
-        },
-        white: {
-          DEFAULT: "#fff",
-          trans: {
-            100: "#FFFFFF25",
-            200: "#FFFFFFEE",
+            20: "var(--accent-color-20)",
+            30: "var(--accent-color-30)",
+            69: "var(--accent-color-69)",
           },
         },
       },
