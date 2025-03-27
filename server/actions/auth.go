e8fbf4b310c99014dd603dd219db69a570f11828
Baraa Al-Masri <pub@mbaraa.com>
2025-03-27 04:05:42 +0300

    chore(auth): cache user session tokens for blazingly fast auth

--- server/actions/auth.go
+++ server/actions/auth.go
@@ -14,9 +14,17 @@ func (a *Actions) AuthenticateUser(sessionToken string) (AuthenticateUserPayload
 		return AuthenticateUserPayload{}, err
 	}
 
-	profile, err := a.app.GetProfileByAccountEmail(token.Payload.Email)
+	profile, err := a.cache.GetAuthenticatedUser(sessionToken)
 	if err != nil {
-		return AuthenticateUserPayload{}, err
+		profile, err = a.app.GetProfileByAccountEmail(token.Payload.Email)
+		if err != nil {
+			return AuthenticateUserPayload{}, err
+		}
+
+		err = a.cache.SetAuthenticatedUser(sessionToken, profile)
+		if err != nil {
+			return AuthenticateUserPayload{}, err
+		}
 	}
 
 	return AuthenticateUserPayload{
--- server/actions/cache.go
+++ server/actions/cache.go
@@ -1,6 +1,12 @@
 package actions
 
+import "dankmuzikk/app/models"
+
 type Cache interface {
 	StoreLyrics(songId uint, lyrics []string) error
 	GetLyrics(songId uint) ([]string, error)
+
+	SetAuthenticatedUser(sessionToken string, profile models.Profile) error
+	GetAuthenticatedUser(sessionToken string) (models.Profile, error)
+	InvalidateAuthenticatedUser(sessionToken string) error
 }
