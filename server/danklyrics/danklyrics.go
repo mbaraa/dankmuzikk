51bbd469ad3b7a17ecdd47db952f58ed73addb7b
Baraa Al-Masri <pub@mbaraa.com>
2025-06-22 01:06:51 +0300

    Wooshh

---
+++ server/danklyrics/danklyrics.go
@@ -0,0 +1,46 @@
+package danklyrics
+
+import (
+	"dankmuzikk/config"
+
+	"github.com/mbaraa/danklyrics/pkg/client"
+	"github.com/mbaraa/danklyrics/pkg/provider"
+)
+
+type dankLyrics struct {
+	client *client.Http
+}
+
+func New() *dankLyrics {
+	client, _ := client.NewHttp(client.Config{
+		Providers:  []provider.Name{provider.Dank, provider.LyricFind},
+		ApiAddress: config.Env().DankLyricsAddress,
+	})
+
+	return &dankLyrics{
+		client: client,
+	}
+}
+
+func (d *dankLyrics) GetForSong(songName string) ([]string, map[string]string, error) {
+	lyrics, err := d.client.GetSongLyrics(provider.SearchParams{
+		SongName: songName,
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return lyrics.Parts, lyrics.Synced, nil
+}
+
+func (d *dankLyrics) GetForSongAndArtist(songName, artistName string) ([]string, map[string]string, error) {
+	lyrics, err := d.client.GetSongLyrics(provider.SearchParams{
+		SongName:   songName,
+		ArtistName: artistName,
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return lyrics.Parts, lyrics.Synced, nil
+}
